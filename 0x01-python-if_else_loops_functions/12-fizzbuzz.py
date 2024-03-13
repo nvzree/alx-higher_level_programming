b21ca8b3e553154f3e2956a0b27b88dfe3b13d73
nvzree <davidnazree@gmail.com>
2024-03-13 19:23:14 +0000

    initial commit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,8 +4,8 @@ def fizzbuzz():
         if (i % 15 == 0):
             print('FIzzBuzz ', end='')
         elif i % 5 == 0:
-            print('Fizz ', end='')
-        elif i % 3 == 0:
             print('Buzz ', end='')
+        elif i % 3 == 0:
+            print('Fizz ', end='')
         else:
             print('{} '.format(i), end='')
