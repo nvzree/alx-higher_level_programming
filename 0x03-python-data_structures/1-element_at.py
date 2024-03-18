ead752c6402ba31b71903d3c3a42a5d7f836a9a3
nvzree <davidnazree@gmail.com>
2024-03-18 13:14:42 +0000

    initial commit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 def element_at(my_list, idx):
     '''function that retrieves an element from a list'''
     if idx < 0:
-       return None
+        return None
     elif idx > (len(my_list) - 1):
-       return None
+        return None
     else:
-       return (my_list[idx])
+        return (my_list[idx])
