bd79d25e36ef347450067abd09f1be7451c81422
nvzree <davidnazree@gmail.com>
2024-03-22 17:09:00 +0000

    initial commit

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def simple_delete(a_dictionary, key=""):
+    '''function that deletes a key in a dictionary.'''
+    if a_dictionary is not None:
+        if key in a_dictionary:
+            del a_dictionary[key]
+            return (a_dictionary)
+        else:
+            return (a_dictionary)
+    else:
+        return None
