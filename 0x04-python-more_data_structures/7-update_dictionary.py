daecd8ebb8fec030c236fd0dd58f0c679fb29085
nvzree <davidnazree@gmail.com>
2024-03-22 16:50:38 +0000

    initial commit

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    ''' replaces or adds key/value in a dictionary.'''
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
+    return (a_dictionary)
